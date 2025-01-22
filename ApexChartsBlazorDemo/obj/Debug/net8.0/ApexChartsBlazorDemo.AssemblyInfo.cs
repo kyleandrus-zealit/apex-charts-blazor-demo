0adbca0f7b17f670523ac8ea55439bd8f8c04294
Kyle Andrus <kyle.andrus@zealitconsultants.com>
2025-01-21 16:01:16 -0800

    Add some POCs for expected value labeling

--- ApexChartsBlazorDemo/obj/Debug/net8.0/ApexChartsBlazorDemo.AssemblyInfo.cs
+++ ApexChartsBlazorDemo/obj/Debug/net8.0/ApexChartsBlazorDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApexChartsBlazorDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8ea0d26affc655000ca92785ad0b421b6116997")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApexChartsBlazorDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApexChartsBlazorDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
